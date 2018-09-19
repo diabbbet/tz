6a4a3daa9e75f33f615299c918498b32e2ba97d8
diabbbet <diabbbet@gmail.com>
2018-09-19 13:09:48 +0400

    fixex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FirstPageComponent } from './first-page/first-page.component';
-import { SecondPageComponent } from './second-page/second-page.component';
+import { FirstPageComponent } from './components/first-page/first-page.component';
+import { SecondPageComponent } from './components/second-page/second-page.component';
 import { UserService } from './services/user.service';
 
 const appRoutes: Routes = [
